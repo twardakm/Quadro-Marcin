2770b76af09d6650ad310bcf956d0ee28eeea5f9
Marcin Twardak <twardakm@gmail.com>
2014-03-09 11:26:26 +0100

    Zainicjalizowanie projektu
    kompiluje się, włącznie z SystemInit()

---
+++ main.c
@@ -0,0 +1,17 @@
+//automatycznie wygenerowany przez CoIDE, ja tylko includujê
+#include "cmsis_boot/stm32f10x.h"
+#include "cmsis/core_cm3.h"
+#include "cmsis_boot/system_stm32f10x.h"
+#include "stm_lib/inc/stm32f10x_gpio.h"
+#include "stm_lib/inc/stm32f10x_i2c.h"
+#include "stm_lib/inc/stm32f10x_rcc.h"
+//----------------------------------------------------------
+
+
+int main(void)
+{
+	SystemInit();
+    while(1)
+    {
+    }
+}
